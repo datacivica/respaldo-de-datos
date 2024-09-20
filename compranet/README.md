ea407dec04551e88c3b2dc5387a9d9c8f9ff73be
basem251 <basem.basem.bb1@gmail.com>
2024-09-20 11:22:01 -0600

    refactor script-fix process bug-adderror handlers

--- compranet/README.md
+++ compranet/README.md
@@ -1,44 +1,57 @@
 # Respaldo-de-datos
 
-## installation
+Este script está diseñado para realizar web scraping de URLs listadas en tablas CSV o XLSX. Utiliza playwright y asyncio para manejar múltiples URLs de forma concurrente, lo que mejora significativamente la velocidad del proceso de scraping.
+
+El script no solo extrae datos de las URLs, sino que también utiliza el paquete playwright para navegar e interactuar con las páginas web. Esto le permite descargar todos los archivos vinculados en estos sitios. Los métodos querySelector y click del paquete playwright se utilizan para localizar y iniciar la descarga de cada enlace de archivo en el sitio.
+
+Este script es especialmente útil para tareas que involucran scraping de múltiples URLs y descarga de archivos, como la recopilación de datos para investigación o análisis, o la automatización de la descarga de archivos de sitios web. Al utilizar programación asíncrona y el paquete playwright, el script puede manejar un gran número de URLs y archivos de manera eficiente y rápida.
+
+## Capacidades
+
+El tiempo de ejecución de este script puede variar significativamente dependiendo de varios factores, incluyendo las capacidades del dispositivo utilizado, la velocidad y la fiabilidad de la conexión a Internet, y el tamaño de los datos que se están raspando.
+
+Para gestionar el número de conexiones simultáneas y optimizar el rendimiento, este script utiliza un método de semáforo de la biblioteca asyncio. Un semáforo es un primitivo de sincronización que limita el número de tareas concurrentes que se pueden ejecutar. De forma predeterminada, el script utiliza un tamaño de semáforo de 10 para limitar el número de conexiones simultáneas a la piscina. Esto significa que hasta 10 URLs se rascarán simultáneamente en cualquier momento.
+
+El tamaño del semáforo se puede ajustar para satisfacer las necesidades específicas de la tarea y los recursos disponibles. El tamaño del semáforo se puede cambiar utilizando la bandera `--sem`, seguida del número deseado de semáforos, que va de 1 a 10. Un tamaño de semáforo mayor permitirá más conexiones simultáneas, lo que puede resultar en tiempos de raspado más rápidos, pero también puede poner una mayor carga en los recursos del dispositivo y la conexión a Internet. Por el contrario, un tamaño de semáforo más pequeño resultará en tiempos de raspado más lentos, pero será más amigable con los recursos y menos propenso a causar problemas con la conexión a Internet.
+
+En última instancia, el tamaño de semáforo óptimo dependerá de los requisitos específicos de la tarea y los recursos disponibles. Es posible que sea necesario experimentar con diferentes tamaños de semáforo para determinar el mejor equilibrio entre velocidad, uso de recursos y fiabilidad.
+
+## instalaciónes
 
 ```env
 python3 -m venv env
+```
+
+```activate
+source env/bin/activate
 ```
 
 ```pip
 pip3 install -r requirements.txt
 ```
 
-## Usage
+## Argumentos
+
+`-h --help`: Muestra esta pantalla de uso.
+`file_path`: La ruta al archivo que desea procesar. Debe ser una cadena que especifique la ubicación del archivo en su sistema.
+`file_format`: El formato del archivo con el que está trabajando. Debe ser 'csv' o 'xlsx', lo que indica si el archivo es un archivo de valores separados por comas o una hoja de cálculo de Excel.
+`name`: Nombre de referencia.
+
+## Opciones
+
+`--sem=<sem>`: Esta bandera se utiliza para gestionar el número de conexiones simultáneas y optimizar el rendimiento. El valor de `<sem>` representa el número máximo de conexiones simultáneas que el script realizará al servidor. Esto puede ayudar a evitar la sobrecarga del servidor y mejorar la velocidad del proceso de scraping. Por defecto, `10`.
+`--col=<column_name>`: Esta bandera se utiliza para el nombre de la columna de URL en pandas DataFrame.
+`--outputcsv=<csv>`: Esta bandera se utiliza para especificar el nombre del archivo de datos de salida. El valor de `<csv>` debe ser una cadena que represente el nombre deseado del archivo de salida, incluyendo la extensión del archivo (por ejemplo, `--outputcsv=my_data.csv`). Por defecto, `output_<name>.csv`.
+`--downloads_path=<downloads_path>`: Esta bandera se utiliza para especificar la ruta donde se guardarán los archivos que se descargan durante el proceso de scraping. El valor de `<downloads_path>` debe ser una cadena que represente la ruta de archivo deseada (por ejemplo, `--downloads_path=/home/user/downloads`). Por defecto, `your_project_path/downloads`.
+
+## Uso
 
 ```Usage:
-  main.py <file_path> <file_format> <start_index> <end_index> <dev_name>
-
-Arguments:
-    -h --help
-
-    file_path:     The path to the file that you want to process. This should be a string specifying the location of the file on your system.
-
-    file_format:   The format of the file you are working with. This should be either 'csv' or 'xlsx', indicating whether the file is a comma-separated values file or an Excel spreadsheet.
-
-    start_index:   The starting index of the range within the DataFrame from which you will begin scraping URLs. This should be an integer indicating the first row of the DataFrame to process.
-
-    end_index:     The ending index of the range within the DataFrame where you will stop scraping URLs. This should be an integer indicating the last row of the DataFrame to process, type all if you want all indexes.
-
-    dev_name:      Your name
-
+Uso:
+  python3 main.py file_path file_format name --sem=sem --output_file_name=output_file_name --downloads_path=downloads_path --col=column_name
 ```
 
-## Merge all outputs files
+## Apoyo
 
-```Usage:
-  merge_data.py <outputFolder_path> <dev_name>
-
-Arguments:
-    -h --help
-
-    outputFolder_path:     Where you want to save Merge csv and xlsx files
-    dev_name:              Your name
-
-```
+Jorge Andrade:<jorge.andrade@datacivica.org>
+Basem Hamza: <basem.hamza@datacivica.org>
--- compranet/main.py
+++ compranet/main.py
@@ -1,165 +1,56 @@
-#######################################################
-############### ORG: DataCivica ##################
-#######################################################
-############### Author: Basem Hamza ##################
-#######################################################
-#######################################################
 """
 Usage:
-  main.py <file_path> <file_format> <start_index> <end_index> <dev_name>
+  main.py <file_path> <file_format> <name> [--sem=<sem>] [--output_file_name=<output_file_name>] [--downloads_path=<downloads_path>] [--col=<column_name>]
 
 Arguments:
     -h --help
     
-    file_path:     The path to the file that you want to process. This should be a string specifying the location of the file on your system.
+    file_path:      The path to the file that you want to process. This should be a string specifying the location of the file on your system.
 
-    file_format:   The format of the file you are working with. This should be either 'csv' or 'xlsx', indicating whether the file is a comma-separated values file or an Excel spreadsheet.
-
-    start_index:   The starting index of the range within the DataFrame from which you will begin scraping URLs. This should be an integer indicating the first row of the DataFrame to process.
-
-    end_index:     The ending index of the range within the DataFrame where you will stop scraping URLs. This should be an integer indicating the last row of the DataFrame to process, type all if you want all indexes.
+    file_format:    The format of the file you are working with. This should be either 'csv' or 'xlsx', indicating whether the file is a comma-separated values file or an Excel spreadsheet.
     
-    dev_name:      Your name
+    name:           Name for reference
+Options:
+
+    --sem=<sem>:               This flag is used to manage the number of concurrent connections and optimize performance. The value of <sem> represents the maximum number of simultaneous connections that the script will make to the server. This can help to prevent overloading the server and improve the speed of the scraping process.
+    
+    --col=<column_name>:       This flag is used the name of the urls column in pandas DataFrame.
+
+    --output_file_name=<output_file_name>:   This flag is used to specify the name of the output data file. The value of <csv> should be a string that represents the desired name of the output file, including the file extension (e.g. --output_file_name=my_data.csv).
+
+    --downloads_path=<downloads_path>: This flag is used to specify the path where the files that are downloaded during the scraping process will be saved. The value of <downloads_path> should be a string that represents the desired file path (e.g. --downloads_path=/home/user/downloads).
     
 """
-#######################################################
-#######################################################
 
-import logging
-from time import sleep
+from scrapingDataFrame import ScrapingDataFrame
 from docopt import docopt
-import numpy as np
-import pandas as pd
-import asyncio
-import nest_asyncio
-from requests_html import AsyncHTMLSession
-import os
-
-arguments = docopt(__doc__)
-#######################################################
-##################... Arguments Input ...##################
-#######################################################
-file_path = arguments["<file_path>"]
-file_format = arguments["<file_format>"]
-dev_name = arguments["<dev_name>"]
-start_index = arguments["<start_index>"]
-end_index = arguments["<end_index>"]
-start_index = int(start_index)
-
-if end_index == "all":
-    end_index = -1
-else:
-    end_index = int(end_index)
-#######################################################
-############### logging basic Config ##################
-#######################################################
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-)
-logger = logging.getLogger("fetch_html_logger")
-file_handler = logging.FileHandler("fetch_html.log")
-file_handler.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-
-#######################################################
-#######################################################
-############... async fetch_html() ...#################
-#######################################################
-#######################################################
-
-nest_asyncio.apply()
 
 
-async def fetch_html(session, url):
-    try:
-        response = await session.get(url)
-        await response.html.arender(sleep=3)
-        body_content = response.html.find("body", first=True).html
-        logger.info(f"Successfully fetched content for {url}")
-        return body_content
-    except Exception as e:
-        logger.error(f"Error fetching {url}: {e}")
-        body_content = f"Fetching Error"
-        return body_content
-
-
-async def fetch_batch_html(urls, batch_size):
-    session = AsyncHTMLSession()
-    tasks = [fetch_html(session, url) for url in urls[:batch_size]]
-    results = await asyncio.gather(*tasks)
-    await session.close()
-
-    return results
-
-
-# Function asyncio to process URLs
-async def process_urls_in_batches(urls, batch_size):
-    print("Session Start ..................-_-..................")
-    all_results = []
-    for i in range(0, len(urls), batch_size):
-        batch_urls = urls[i : i + batch_size]
-        print(
-            f"Processing batch {i // batch_size + 1} of {(len(urls) + batch_size - 1) // batch_size}"
-        )
-        batch_results = asyncio.run(fetch_batch_html(batch_urls, batch_size))
-        all_results.extend(batch_results)
-    return all_results
-
-
-def main(file_path, file_format, dev_name):
+def main():
+    arguments = docopt(__doc__)
     #######################################################
+    ##################... Arguments Input ...##################
     #######################################################
-    ##########... extract URL from dataFrame ...###########
-    #######################################################
-    #######################################################
-    if file_format == "xlsx":
-        df = pd.read_excel(f"{file_path}", sheet_name="Sheet1")
-    if file_format == "csv":
-        df = pd.read_csv(f"{file_path}")
+    file_path = arguments["<file_path>"]
+    file_format = arguments["<file_format>"]
+    name = arguments["<name>"]
+    sem = arguments["--sem"]
+    output_file_name = arguments["--output_file_name"]
+    downloads_path = arguments["--downloads_path"]
+    column_name = arguments["--col"]
 
-    if "Dirección del anuncio" not in df.columns:
-        raise ValueError("The DataFrame does not contain a 'url' column.")
-
-    ########################################################################
-    ########################################################################
-    ############... Create Chunks por 10 row for each chunk ...#############
-    ########################################################################
-    ########################################################################
-    chunks = np.array_split(
-        df.iloc[start_index:end_index], (len(df.iloc[start_index:end_index]) + 9) // 10
+    sem = int(sem) if sem is not None else 10
+    scraping = ScrapingDataFrame(
+        file_path=file_path,
+        file_format=file_format,
+        name=name,
+        output_file_name=output_file_name,
+        sem=sem,
+        downloads_path=downloads_path,
+        column_name=column_name,
     )
-    for i, chunk in enumerate(chunks):
-        urls = chunk["Dirección del anuncio"].tolist()
-
-        #######################################################
-        ############... Run Function ...#################
-        #######################################################
-
-        html_contents = asyncio.run(process_urls_in_batches(urls, len(chunk)))
-
-        ################# Add a new column html ##############
-
-        chunk["html"] = html_contents
-
-        #######################################################
-        ################# Save Excel file and CSV ############
-        #######################################################
-        if not os.path.exists(
-            os.path.join(f"outputs_folders/{start_index}_{end_index}/")
-        ):
-            os.makedirs(os.path.join(f"outputs_folders/{start_index}_{end_index}"))
-            os.makedirs(os.path.join(f"outputs_folders/{start_index}_{end_index}/csv"))
-
-        chunk.to_csv(
-            f"outputs_folders/{start_index}_{end_index}/csv/Expedientes_PICompraNet2024_{dev_name}_output_file{i}.csv",
-            index=False,
-        )
-        os.system("killall 'Chromium'")
-        print(f"Session number {i} end .......o_o.........o_o.......o_o........")
-        sleep(7)
+    scraping.main()
 
 
 if __name__ == "__main__":
-    main(file_path=file_path, file_format=file_format, dev_name=dev_name)
+    main()
